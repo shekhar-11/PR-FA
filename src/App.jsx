021b02c0623ea24ac3987a1721774eb80ef96016
shekhar-11 <rajshekhar111201@gmail.com>
2025-11-09 19:45:03 +0530

    info box creation

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import PRpage from './PRpage'
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <PRpage/>
+    </>
+  )
+}
+
+export default App
